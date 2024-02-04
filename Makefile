29430e61e74b1c7a873bbb1a7bc0ee20d141c05d
mfinette <maxime.finette1@gmail.com>
2024-02-04 05:51:43 -0500

    almost finished?

--- Makefile
+++ Makefile
@@ -1,5 +1,9 @@
 include srcs/.env
 
+DOCKER_COMPOSE := docker compose -f srcs/docker-compose.yml
+_END			:=	\033[0m
+_GREEN			:=	\033[32m
+
 all:	$(WP_VOLUME_PATH) $(MARIADB_VOLUME_PATH)
 		docker compose -f ./srcs/docker-compose.yml up --build
 
@@ -11,7 +15,7 @@ re:		clean
 
 clean:
 		docker compose -f srcs/docker-compose.yml down --volumes --rmi all
-		rm -rf $(WP_VOLUME_PATH) $(MARIADB_VOLUME_PATH)
+		sudo rm -rf $(WP_VOLUME_PATH) $(MARIADB_VOLUME_PATH)
 
 $(WP_VOLUME_PATH):
 		mkdir -p $(WP_VOLUME_PATH) 
@@ -19,4 +23,18 @@ $(WP_VOLUME_PATH):
 $(MARIADB_VOLUME_PATH):
 		mkdir -p $(MARIADB_VOLUME_PATH)
 
-.PHONY: all re down clean fclean
+ls:
+	@echo "$(_GREEN)------------------------List running containers-------------------------$(_END)"
+	$(DOCKER_COMPOSE) ps
+	@echo "$(_GREEN)------------------------------List images-------------------------------$(_END)"
+	docker images
+	@echo "$(_GREEN)------------------------------List volumes------------------------------$(_END)"
+	docker volume ls
+
+prune: clean
+	@echo "$(_GREEN)Removes all unused images, containers, networks and volumes$(_END)"
+	sudo docker system prune -f -a
+# -f = force the removal without confirmation
+# -a = remove all objects including unused
+
+.PHONY: all re down clean fclean ls prune
---
+++ srcs/requirements/wordpress/conf/wp-config.sh
@@ -0,0 +1,57 @@
+# !/bin/bash
+
+# Execution du script si wp-config n'existe pas
+if [ ! -f "${WP_PATH}/wp-config.php" ]; then
+
+  # Installation de Wordpress
+  wp core download --path=$WP_PATH --allow-root
+  echo "core downloaded" >&2
+  sleep 10
+  # Configuration de Wordpress avec des variables d'environnement
+  wp config create --allow-root \
+    --dbname=$SQL_DATABASE \
+    --dbuser=$SQL_USER \
+    --dbpass=$SQL_PWD \
+    --dbhost=mariadb:3306 \
+    --path=$WP_PATH
+  echo "wp-config.php created"
+  # # Configure WordPress to use SMTP for sending emails
+  # wp config set --allow-root \
+  #   --type=constant \
+  #   --raw \
+  #   WP_MAIL_HOST smtp.example.com
+  # wp config set --allow-root \
+  #   --type=constant \
+  #   --raw \
+  #   WP_MAIL_PORT 587
+  # wp config set --allow-root \
+  #   --type=constant \
+  #   --raw \
+  #   WP_MAIL_USERNAME your_smtp_username
+  # wp config set --allow-root \
+  #   --type=constant \
+  #   --raw \
+  #   WP_MAIL_PASSWORD your_smtp_password
+  # wp config set --allow-root \
+  #   --type=constant \
+  #   --raw \
+  #   WP_MAIL_SECURE tls
+  echo "Setting up admin" >&2
+  wp core install --allow-root \
+    --url="${WP_URL}" \
+    --title="${WP_TITLE}" \
+    --admin_user="${WP_ADMIN_USER}" \
+    --admin_password="${WP_ADMIN_PWD}" \
+    --admin_email="${WP_ADMIN_EMAIL}" \
+    --path=$WP_PATH
+  echo "Setting up user" >&2
+  wp user create $WP_USER $WP_USER_EMAIL \
+    --user_pass=$WP_USER_PWD \
+    --allow-root \
+    --path=$WP_PATH
+
+fi
+
+# Run PHP
+mkdir -p /run/php
+php-fpm7.4 -F
